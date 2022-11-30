bc1d6ad1f24e7ff5219fdb9114b3493a62ef875b
hichamDao <webnetmaster01@gmail.com>
2022-11-30 23:17:30 +0000

    Train Example

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const xs=tf.tensor2d([[0,0],[0.5,0.5],[1,1]]);
 const ys=tf.tensor1d([1,0.5,0]);
 
 const model=tf.sequential();
-const learningRate=0.01;
-const numberOfEpochs=100;
+const learningRate=0.1;
+const numberOfEpochs=800;
 const optimizer=tf.train.adam(learningRate);
 
 model.add(tf.layers.dense({
     units:5,activation:'sigmoid',inputShape:[2]
 }));
 model.add(tf.layers.dense({
-    units:1,activation:'softmax'
+    units:1,activation:'sigmoid'
 }));
 
 model.compile({
@@ -23,16 +23,13 @@ model.compile({
 
 model.fit(xs,ys,{
     epochs:numberOfEpochs,
-    //validationData:[xsTest,ysTest],
     callbacks:{
         onEpochEnd:logProrgess
     }
-}).then(resp=>{
-   console.log(resp.history.loss[0]);
-   console.log(resp.history.acc);
-   predict();
+}).then(
+   predict
    //testModel();
-});
+);
 
 async function  logProrgess(epoch,logs){
     console.log("epoch :"+epoch+" , Loss:"+logs.loss);
@@ -40,10 +37,10 @@ async function  logProrgess(epoch,logs){
 }
 
 function predict(){
-    let data=[0.5,0.5]
+    console.log("Model trained")
+    let data=[0,0]
     const input=tf.tensor2d([data],[1,2]);
     let prediction=model.predict(input);
-    alert(prediction);
     //const index=model.predict(input).argMax(-1).dataSync();
     let output= prediction.dataSync()[0];
 
